f9cba81dc3d70f902cc370c03463418d7aac9b99
tehdb <xac.ini@gmail.com>
2013-03-03 23:03:35 +0100

    navi + breadcrumb

--- public/js/tehdb.js
+++ public/js/tehdb.js
@@ -1,8 +1,11 @@
 $(function () {
-
 	function blenderAni(event) {
 		var $this = $(this);
-		$this.unbind();
+		$this.unbind('mouseenter');
+		$this.unbind('mouseleave');
+		if($this.hasClass('active')) {
+			return false;
+		}
 		var $li = $this.parent();
 		var $blender = null;
 		var bw = $this.width();
@@ -32,7 +35,36 @@ $(function () {
 		});
 	}
 
-	$('#TH-main-navi .button').mouseenter(blenderAni);
+	$('#TH-main-navi .button').mousedown(function(event){
+		var $this = $(this);
+		event.stopPropagation();
+		event.preventDefault();
+		if($this.hasClass('active')){
+			return false;
+		}
+		var anidur = 222;
+
+		$this.animate( {
+			'padding-top' : 55
+		}, anidur, 'linear', function(){
+			$this.unbind("click");
+			$this.trigger("click");
+		});
+
+		$('#TH-main-navi .active').animate({
+			'padding-top' : 85
+		}, anidur);
+
+	}).mouseup(function(event){
+		event.stopPropagation();
+		event.preventDefault();
+		$(this).addClass('active');
+	}).mouseenter(blenderAni).click(function(event){
+		event.stopPropagation();
+		event.preventDefault();
+	});
+
+
 
 
 	$('.f-dropdown').mouseleave(function(event){
